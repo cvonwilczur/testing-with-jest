8f8a67e498d6f0b823cf5211d01f43b911dfb8de
Christopher Von Wilczur <cvwilczur@zenefits.com>
2018-11-08 14:52:46 -0700

    additional work with tests

--- script.test.js
+++ script.test.js
@@ -8,11 +8,22 @@ dbMock = [
   'dogpictures.com'
 ];
 
-it('this is a silly test', () => {
-  expect('hello').toBe('hello');
-})
+describe('googleSearch', () => {
+  it('is a silly test', () => {
+    expect('hello').toBe('hello');
+  })
 
-it('is searching Google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([])
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  it('is searching Google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  })
+
+  it('works with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  })
+
+  it('does not return more than three matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  })
 })
